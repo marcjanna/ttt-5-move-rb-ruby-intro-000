227fbf68f9afa9830404c21e237c6f7f1cd34d54
Marcjanna Mietla <mietla.m@gmail.com>
2020-04-16 18:43:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  converted_input = user_input.to_i - 1
+  index = user_input.to_i - 1
 #expect(converted_input).to be_a(Integer)
 end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
